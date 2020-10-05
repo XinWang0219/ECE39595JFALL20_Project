44a028fafa7855f2fa7fc8cea5d413b821fba0e9
XinWang0219 <51687272+XinWang0219@users.noreply.github.com>
2020-10-05 00:19:58 -0400

    Delete ChangeDisplayedType.java
    delete because the wrong file name

--- ChangeDisplayedType.java
+++
@@ -1,5 +0,0 @@
-public class ChangeDisplayedType extends CreatureAction{
-    public ChangeDisplayedType(String name, Creature owner){
-        System.out.println("CreatureAction ChangeDisplayedType-> name:"+name+"; owner: "+owner.name);
-    }
-}
